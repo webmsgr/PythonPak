35933beee232db53f56d3a15272e05d5c2a6d53f
webmsgr <32279125+webmsgr@users.noreply.github.com>
2017-12-12 17:52:13 -0800

    Update Example.py

--- Example.py
+++ Example.py
@@ -8,16 +8,17 @@ def search(path,isnotrec=True):
         ret = []
     for root, dirs, files in os.walk(path):
         for e in files:
-            if (e.endswith(".pkg")):
+            if (e.endswith(".py")):
                 ret.append(os.path.join(root,e))
         for e in dirs:
             search(os.path.join(root, e),True)
     if (isnotrec):
         return ret
+print("Making all .py files into a single build.pkg file")
 print("searching")
-s = search(os.path.realpath("C:\\Users\\User\\Desktop"))
+s = search(os.path.realpath("C:\\")) #search
 print("Building Pakage")        
 pkg.Make(s,"build.pkg")
 print("Extraction")
-pkg.breakpkg("build.pkg",os.path.realpath(".\py"))
+pkg.breakpkg("build.pkg",os.path.realpath(".\py")) #comment out if you dont want to autoextract
                                           
